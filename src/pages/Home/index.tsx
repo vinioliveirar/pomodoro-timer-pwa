e647ad6bb15cf7dd47243264f5891ea0ef7313c3
Vinicius Oliveira <vinioliveirar01@gmail.com>
2025-02-26 09:40:07 -0300

    Configuração inicial do projeto e criação da Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Pomodoro Timer</h1>
+      <p>Gerencie seu tempo de foco e descanso.</p>
+    </div>
+  );
+}
