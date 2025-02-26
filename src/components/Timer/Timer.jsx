86ea12f3ba435a5d093f9bce8040c2f615ed2ca1
Vinicius Oliveira <vinioliveirar01@gmail.com>
2025-02-26 19:50:30 -0300

    Implementa timer com useEffect e setInterval

---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react"
+
+export default function Timer(){
+  const [time, setTime] = useState(25*60);// 25 minutos em segundos
+  const [isRunning, setIsRunning] = useState(false)//controle do time rodando
+
+  useEffect(() =>{
+    let interval;
+
+    if(isRunning){
+      interval = setInterval(()=>{
+        setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : 0))
+      }, 1000)
+    }else{
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval)
+  }, [isRunning])
+
+  function toggleTimer(){
+    setIsRunning(!isRunning);
+  }
+
+  //função que formata o tempo
+  function formatTime(seconds){
+    const minutes = Math.floor(seconds / 60);//arredonda para baixo, deixando num inteiro
+    const secs = seconds % 60;
+    return `${String(minutes).padStart(2, "0")}:${String(secs).padStart(2, "0")}`
+    //garante que os minutos e segundos tenha 2 digitos
+  }
+
+  return(
+    <div>
+      <h2>{formatTime(time)}</h2>
+      <button onClick={toggleTimer}>{isRunning ? "Pausar" : "Iniciar"}</button>
+    </div>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Timer from "../../components/Timer/Timer";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Pomodoro Timer</h1>
-      <p>Gerencie seu tempo de foco e descanso.</p>
+    <div className="flex items-center flex-col justify-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Pomodoro Timer</h1>
+      <Timer />
     </div>
+    
   );
 }
