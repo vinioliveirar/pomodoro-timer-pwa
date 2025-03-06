503fb6d86330863d1c33750d6d668591857dea3f
Vinicius Oliveira <vinioliveirar01@gmail.com>
2025-03-06 20:38:28 -0300

    ajusta todolist

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Timer from "../../components/Timer/Timer";
-import TodoList from "../../components/ToDoList/ToDoList.jsx";
+import TodoList from "../../components/ToDoList/index.jsx";
 
 export default function Home() {
   return (
